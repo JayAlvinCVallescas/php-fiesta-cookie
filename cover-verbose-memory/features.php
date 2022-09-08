58dfc9a38e3853e6ae454929bc990ff85652b96f
Jay Alvin C. Vallescas <202080209@psu.palawan.edu.ph>
2022-09-08 13:46:57 +0800

    inc directory done

---
+++ cover-verbose-memory/features.php
@@ -0,0 +1,12 @@
+<?php include 'inc/header.php'; ?>
+
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Features.</h1>
+        <p class="lead">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+             voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.
+        </p>
+      </main>
+
+<?php include 'inc/footer.php'; ?>
