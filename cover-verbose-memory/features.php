b720d72e92a244f1fcb184eedf10d30a7067b830
gillbertmpadon <202080194@psu.palawan.edu.ph>
2022-09-11 09:48:21 +0800

    inc directory done

--- cover-verbose-memory/features.php
+++ cover-verbose-memory/features.php
@@ -3,7 +3,7 @@
       <main role="main" class="inner cover">
         <h1 class="cover-heading">Features.</h1>
         <p class="lead">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
-            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
+            inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. 
             Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
              voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.
         </p>
