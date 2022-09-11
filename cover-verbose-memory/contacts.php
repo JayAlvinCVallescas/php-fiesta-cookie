3dca7361e117d5a1a18dec17c3d435f69bcfcaba
Jay Alvin C. Vallescas <202080209@psu.palawan.edu.ph>
2022-09-11 13:33:20 +0800

    inc directory done

--- cover-verbose-memory/contacts.php
+++ cover-verbose-memory/contacts.php
@@ -3,7 +3,7 @@
       <main role="main" class="inner cover" >
         <h1 class="cover-heading">Contacts.</h1>
         <p class="lead">pv@gmail.com</p>
-        <p class="lead" style="margin: 0 12.5em;">09981190251</p>
+        <p class="lead" style="margin: 0 12.5em;">09981190252</p>
       </main>
 
 <?php include 'inc/footer.php'; ?>
